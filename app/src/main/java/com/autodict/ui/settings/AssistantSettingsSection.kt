c54aaf957a39d15a6751322fb7fb04852a4b25ba
Håkon Hole Lønning <hhl1987@hotmail.com>
2026-08-27 21:22:42 +0000

    Fiks import: KeyboardOptions ligg i foundation.text, ikkje ui.text.input

--- app/src/main/java/com/autodict/ui/settings/AssistantSettingsSection.kt
+++ app/src/main/java/com/autodict/ui/settings/AssistantSettingsSection.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -22,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.unit.dp
 import com.autodict.data.assistant.AssistantPreset
 
